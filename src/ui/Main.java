03a4b191943cec775a9864fa1cc6246acbddb38e
elkofix <holasoygermamwow@gmail.com>
2023-02-10 09:18:21 -0500

    Adding initializeUserQuestions() method for ui and join it in startGame() method

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -9,17 +9,26 @@ public class Main {
     public Main(){
         control =new Controller();
         sc = new Scanner(System.in);
-        askName();
+        startGame();
     }
     public static void main(String[] args){
         Main main = new Main();
 
     }
+
+    public void startGame(){
+        askName();
+        initializeUserQuestions();
+    }
     
     public void askName(){
         System.out.println("Insert your name");
         String name = sc.nextLine();
+    }
 
+    public void initializeUserQuestions(){
+        System.out.println("Insert desired number of questions");
+        int numberQuestions = sc.nextInt();
     }
     
 }
