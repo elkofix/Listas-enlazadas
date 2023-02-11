b9fb5bba3ab7ae6a9fe034c58a46de56ca0d2d89
elkofix <holasoygermamwow@gmail.com>
2023-02-10 21:39:31 -0500

    Organizing Controller methods

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -7,10 +7,6 @@ public class Controller {
     private Question first;
     private Question last;
     private int numQuestions;
-    public int getNumQuestions() {
-        return numQuestions;
-    }
-
     private int userPoints;
 
     public Controller(String id, String name){
@@ -32,28 +28,21 @@ public class Controller {
             this.last=newQuestion;
         }
     }
-    public boolean generateQuestion(int numberQuestions){
-        return generateQuestion(numberQuestions, null, 1);
-    }   
 
-
-    public String printList(){
-        String msj = "";
-        return printlist(first, msj);
+    public Question search(String id) {
+        return search(id, first);
     }
 
-    public void deleteQuestion(String id){
-        Question current = search(id);
-        Question previous = searchPrevious(id);
-        if(previous!=null){
-            previous.setNext(current.getNext());
-            current=null;
-           }else{
-                this.first = current.getNext();
-                current=null;
-           }
-           return;
-       }
+    public Question search(String id, Question current) {
+        if(current==null){
+            return current;
+        }
+        if(current.getId().equals(id)){
+            return current;
+        }
+        current = current.getNext();
+        return search(id, current);
+    }
 
     public Question searchPrevious(String id) {
         return searchPrevious(id, first, last);
@@ -68,14 +57,18 @@ public class Controller {
         return searchPrevious(id, current, previous);
     }
 
-    private String printlist(Question current, String list){
-        if(current==null){
-            return list;
-        }
-        list+=current.toString();
-        current = current.getNext();
-        return printlist(current, list);
-    }
+    public void deleteQuestion(String id){
+        Question current = search(id);
+        Question previous = searchPrevious(id);
+        if(previous!=null){
+            previous.setNext(current.getNext());
+            current=null;
+           }else{
+                this.first = current.getNext();
+                current=null;
+           }
+           return;
+       }
 
     public String[] askQuestion(){
         return askQuestion(first, false);
@@ -112,19 +105,8 @@ public class Controller {
         return correct;
     }
 
-    public Question search(String id) {
-        return search(id, first);
-    }
-
-    public Question search(String id, Question current) {
-        if(current==null){
-            return current;
-        }
-        if(current.getId().equals(id)){
-            return current;
-        }
-        current = current.getNext();
-        return search(id, current);
+    public boolean generateQuestion(int numberQuestions){
+        return generateQuestion(numberQuestions, null, 1);
     }
 
     public boolean generateQuestion(int numberQuestions, Question addQuestion, int currentQuestion){
@@ -141,7 +123,22 @@ public class Controller {
         this.numQuestions++;
         return generateQuestion(numberQuestions, addQuestion, currentQuestion);
 
+    }  
+    public String printList(){
+        String msj = "";
+        return printlist(first, msj);
     }
+
+
+    private String printlist(Question current, String list){
+        if(current==null){
+            return list;
+        }
+        list+=current.toString();
+        current = current.getNext();
+        return printlist(current, list);
+    }
+
 
     public int giveResult(int n, int m, int operation){
         int result = 1;
@@ -158,7 +155,6 @@ public class Controller {
 
     }
 
-    
     public String giveStatement(int n, int m, int operation){
         String statement = "";
         if(operation==1){
@@ -187,6 +183,10 @@ public class Controller {
 
     public void setFirst(Question first) {
         this.first = first;
+    }
+
+    public int getNumQuestions() {
+        return numQuestions;
     }
 
 }
